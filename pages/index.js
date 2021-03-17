0082dfdf8f9eeb739015017aa1c29f4fafd00e12
nathanclevenger <nateclev@gmail.com>
2021-03-17 04:30:45 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <Page dotBackdrop size="mini">
       <Page.Header>
-        <Text h2>React Application with Geist UI</Text>
+        <Text h2>Convert CURL commands to JavaScript</Text>
       </Page.Header>
       <Text>
         Hello, I am using <Text b>Geist UI</Text> !
